dc968fae80fe42a5d7c3410d35b849ad1df92771
Jacob Smith <jacob@testdouble.com>
2018-10-15 17:09:30 -0400

    Add Screenflow 6

---
+++ Casks/screenflow6.rb
@@ -0,0 +1,13 @@
+cask 'screenflow6' do
+  version '6.2.4'
+  sha256 '8cee4493bf45b9f2dea24abf56ffac2457f86260138118363fad68cbf200cc50'
+
+  url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
+  name 'ScreenFlow'
+  homepage 'https://www.telestream.net/screenflow/'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
+  app 'ScreenFlow.app'
+end
